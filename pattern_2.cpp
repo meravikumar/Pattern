1cba729baf6f901b0e6fb4ea08fb7f7bd0ece1ee
Ravi kumar <69406930ravi@gmail.com>
2022-10-22 16:37:13 +0530

    Number Pattern2

---
+++ pattern_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r,c;
+    cin>>r>>c;
+    int i =0;
+    while(i<r){
+        int j=0;
+        while(j<c){
+            cout<<j;
+            j++;
+        }
+    cout<<endl;
+      i++ ;
+    }
+
+    return 0;
+}
+
+
